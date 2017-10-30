b35f658f9f4fad482f0e90482492f399b35bfd78
mwerumuchai <mwerumuchai@gmail.com>
2017-10-30 10:34:47 +0300

    add title to render template

--- app/main/views.py
+++ app/main/views.py
@@ -8,4 +8,5 @@ def index():
     '''
     View root page function that returns the index page and its data
     '''
-    return render_template('index.html')
+    title = 'Home - Welcome to One Minute Pitch'
+    return render_template('index.html', title = title)
