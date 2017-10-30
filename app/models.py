aa7f38f68ed5e7a2ab2c3acc3619f1e873664e05
mwerumuchai <mwerumuchai@gmail.com>
2017-10-30 23:11:13 +0300

    create class category

--- app/models.py
+++ app/models.py
@@ -1,9 +1,27 @@
 from . import db
 
-class User(db.Model):
-    __tablename__ = 'users'
-    id = db.Column(db.Integer,primary_key = True)
-    username = db.Column(db.String(255))
+class Category(db.Model):
+    '''
+    Category class define category per pitch
+    '''
+    __tablename__ = 'categories'
 
-    def __repr__(self):
-        return f'User {self.username}'
+    # add columns
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.string(255))
+
+    # save
+    def save_category(self):
+        '''
+        Function that saves a category
+        '''
+        db.session.add(self)
+        db.session.commit()
+
+    @classmethod
+    def get_categories(cls):
+        '''
+        Function that returns all the data from the categories after being queried
+        '''
+        categories = Category.query.all()
+        return categories
